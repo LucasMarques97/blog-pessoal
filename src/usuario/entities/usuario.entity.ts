11a3fd658c1b8e61070ee6b70a7314754399d1f8
LucasMarques97 <lucaschristian76@gmail.com>
2022-11-10 15:36:36 -0300

    projeto bão

---
+++ src/usuario/entities/usuario.entity.ts
@@ -0,0 +1,37 @@
+import { ApiProperty } from "@nestjs/swagger/dist"
+import { IsEmail, IsNotEmpty, MinLength } from "class-validator"
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm"
+import { Postagem } from "../../postagem/entities/postagem.entity"
+
+@Entity({name: "tb_usuarios"})
+export class Usuario {
+    
+    @PrimaryGeneratedColumn()
+    @ApiProperty()
+    public id: number
+
+    @IsNotEmpty()
+    @Column({length: 255, nullable: false}) 
+    @ApiProperty()
+    public nome: string
+
+    @IsEmail()
+    @MinLength(8)
+    @Column({length: 255, nullable: false })
+    @ApiProperty({example: "email@email.com"})
+    public usuario: string
+
+    @IsNotEmpty()
+    @Column({length: 255, nullable: false }) 
+    @ApiProperty()
+    public senha: string
+
+    @Column({length: 5000 }) 
+    @ApiProperty()
+    public foto: string
+
+    @ApiProperty({ type: () => Postagem})
+    @OneToMany(() => Postagem, (postagem) => postagem.usuario)
+    postagem: Postagem[]
+
+}
